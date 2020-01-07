7fceff72a630e2f39bf17fa709181a6d90abd099
pugongyingh <pgyhh@sina.cn>
2020-01-07 09:55:07 +0800

    Update visit.js

--- functions-src/visit.js
+++ functions-src/visit.js
@@ -4,7 +4,7 @@ exports.handler = (event, context, callback) => {
       host : 'arjuna.db.elephantsql.com',
       port: 5432,
       user : 'juicmaka',
-      password : '	okUGxNKWk6CtRezIOHLBHxPbYiGMiQcS',
+      password : 'okUGxNKWk6CtRezIOHLBHxPbYiGMiQcS',
       database : 'juicmaka',
     };
 
